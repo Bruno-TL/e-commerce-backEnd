3ed0e50e504519a34c43d65d6bf57cf3b71a3b34
Ciro <cbjales@gmail.com>
2022-10-27 17:36:27 -0300

    chore: add user route

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-const produtoRota = require("./controllers/produto/router.js")
-const comentarioRota = require("./controllers/comentario/router.js")
+const produtoRota = require("./controllers/produto/router.js");
+const comentarioRota = require("./controllers/comentario/router.js");
+const usuarioRota = require("./controllers/usuario/router.js");
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req,res)=> {
-    res.send('ta funfando')
-})
+app.get("/", (req, res) => {
+  res.send("ta funfando");
+});
 
-app.use('/', produtoRota)
-app.use('/', comentarioRota)
+app.use("/", produtoRota);
+app.use("/", comentarioRota);
+
+app.use("/", usuarioRota);
 
 app.use((req, res) => {
-    res.status(404).send("{message: rota não encontrada}")
-  })
+  res.status(404).send("{message: rota não encontrada}");
+});
 
-
-
-
-app.listen(port,() => {
-    console.log(`BackEnd rodando na URL= http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`BackEnd rodando na URL= http://localhost:${port}`);
+});
