644c25f0c0f5e23b8ed9873eff595a0cc95c56b3
Ciro <cbjales@gmail.com>
2022-10-31 19:59:30 -0300

    chore: edit files

--- controllers/carrinho/carrinho.js
+++ controllers/carrinho/carrinho.js
@@ -1,82 +1,77 @@
-
-const { application } = require('express');
-const { buscarUsuario } = require('../usuario/usuario');
+const { application } = require("express");
+const { buscarUsuario } = require("../usuario/usuario");
 
 let carrinho = [
-    {
-        id: '1',
-        produto: 'Addidas',
-        usuario: '1',
-        quantidade: '2',
-    },
-    {
-        id: '2',
-        produto: 'Nike',
-        usuario: '2',
-        quantidade: '3',
-    },
-    {
-        id: '3',
-        produto: 'opanca',
-        usuario: '2',
-        quantidade: '1',
-    },
-]
+  {
+    id: "1",
+    produto: "Addidas",
+    usuario: "1",
+    quantidade: "2",
+  },
+  {
+    id: "2",
+    produto: "Nike",
+    usuario: "2",
+    quantidade: "3",
+  },
+  {
+    id: "3",
+    produto: "opanca",
+    usuario: "2",
+    quantidade: "1",
+  },
+];
 
 function carrinhoUsuario(idUsuario) {
+  if (!idUsuario) {
+    return res.send("Usuario não encontrado").status(400);
+  }
 
-    if (!idUsuario) {
-        return res.send('Usuario não encontrado').status(400);
-    }
-
-    let usuario = buscarUsuario(idUsuario);
-    let resultado = carrinho.filter(c => c.usuario == usuario.id);
-    return resultado;
-
+  let usuario = buscarUsuario(idUsuario);
+  let resultado = carrinho.filter((c) => c.usuario == usuario.id);
+  return resultado;
 }
 function addCarrinho(idUsuario, dados) {
-    if (!idUsuario) {
-        return res.send('Não existe esse produto').status(400);
-    }
-    let identificadores = carrinho.map(item => item.id);
-    let novoId = Math.max(...identificadores) + 1;
-    let novoItem = {
-        id: novoId,
-        produto: dados.produto,
-        usuario: idUsuario,
-        quantidade: dados.quantidade
-    }
-    carrinho.push(novoItem);
+  if (!idUsuario) {
+    return res.send("Não existe esse produto").status(400);
+  }
+  let identificadores = carrinho.map((item) => item.id);
+  let novoId = Math.max(...identificadores) + 1;
+  let novoItem = {
+    id: novoId,
+    produto: dados.produto,
+    usuario: idUsuario,
+    quantidade: dados.quantidade,
+  };
+  carrinho.push(novoItem);
 
-    //como não foi pedido apenas o novo, estaremos enviando todos ao mesmo tempo.
-    return (carrinho)
+  //como não foi pedido apenas o novo, estaremos enviando todos ao mesmo tempo.
+  return carrinho;
 }
 
-function attCarrinho(idUsuario, dados){
-    if (!idUsuario) {
-        res.send.status(400)
-        
-    }
+function attCarrinho(idUsuario, dados) {
+  if (!idUsuario) {
+    res.send.status(400);
+  }
 
-    let usuario = buscarUsuario(idUsuario);
-    let resultado = carrinho.filter(c => c.usuario == idUsuario);
-    
-    // itemAtualizado = {
-    //     id: dados.id,
-    //     produto: resultado.produto,
-    //     usuario: resultado.usuario,
-    //     quantidade: dados.quantidade
-    // }
+  let usuario = buscarUsuario(idUsuario);
+  let resultado = carrinho.filter((c) => c.usuario == idUsuario);
 
-    let itemAtualizado = resultado.map(cada => {
-        cada.id = dados.id,
-        cada.quantidade = dados.quantidade
-    })
-    return itemAtualizado
+  // itemAtualizado = {
+  //     id: dados.id,
+  //     produto: resultado.produto,
+  //     usuario: resultado.usuario,
+  //     quantidade: dados.quantidade
+  // }
+
+  let itemAtualizado = resultado.map((cada) => {
+    (cada.id = dados.id), (cada.quantidade = dados.quantidade);
+  });
+  return itemAtualizado;
 }
 
 module.exports = {
-    carrinhoUsuario,
-    addCarrinho,
-    attCarrinho,
+  carrinhoUsuario,
+  addCarrinho,
+  attCarrinho,
 };
--- controllers/usuario/usuario.js
+++ controllers/usuario/usuario.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 function listar() {
   return JSON.parse(fs.readFileSync(__dirname + "/db-usuarios.json")).map(
@@ -19,7 +21,25 @@ function buscarUsuario(usuarioID) {
   return listar().find((usuario) => usuario.id == Number(usuarioID));
 }
 
+function autenticar(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === null) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, usuario) => {
+    console.log(usuario);
+    if (err) {
+      return res.sendStatus(403);
+    }
+    req.usuario = usuario;
+    next();
+  });
+}
+
 module.exports = {
   listar,
   buscarUsuario,
+  autenticar,
 };
